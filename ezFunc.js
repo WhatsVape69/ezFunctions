24225577bea4135e764d9b03b91ef5a4d8d3af99
WhatsVape69 <47509345+WhatsVape69@users.noreply.github.com>
2021-01-11 18:01:10 -0800

    Create ezFunc.js

---
+++ ezFunc.js
@@ -0,0 +1,12 @@
+//reload function
+function reload() {
+  location.reload();
+}
+//go back function
+function goBack() {
+  window.history.back();
+}
+//go forward function
+function goForward() {
+  window.history.forward();
+}
